872d9d6857feda104c5204632711662af348ab85
artn-dev <azemarteixeira@gmail.com>
2020-11-26 17:09:02 -0300

    propriedade direction

--- player.py
+++ player.py
@@ -10,10 +10,14 @@ class Player:
         self._angle         = angle
         self._size          = size
 
-    def update(self, dt):
+    @property
+    def direction(self):
         direction = pg.Vector2(1.0, 0.0)
         direction.rotate_ip(self._angle)
-        self._position += direction * self._speed * dt
+        return direction
+
+    def update(self, dt):
+        self._position += self.direction * self._speed * dt
 
     def rotate(self, dt, angle):
         self._angle += angle * dt
@@ -32,11 +36,9 @@ class Player:
         pg.draw.rect(surface, (0,255,0), [self._position.x, self._position.y, self._size, self._size])
 
         if '-o' not in sys.argv:
-            direction = pg.Vector2(1.0, 0.0)
-            direction.rotate_ip(self._angle)
             length    = 40
             coord1 = self.get_center()
-            coord2 = int(coord1[0] + direction.x * length), int(coord1[1] + direction.y * length)
+            coord2 = int(coord1[0] + self.direction.x * length), int(coord1[1] + self.direction.y * length)
             coord1 = int(coord1[0]), int(coord1[1])
             pg.gfxdraw.line(surface, *coord1, *coord2, (223, 32, 203))
 
