a37f81ac80ef7ac63e929c3db7f8552164701bce
artn-dev <azemarteixeira@gmail.com>
2020-12-02 17:22:46 -0300

    integracao codigo de Lucca

--- core/ship.py
+++ core/ship.py
@@ -1,71 +1,39 @@
 import sys
 import pygame as pg
-from pygame import gfxdraw
+from .battery import Battery
 
 
 class Ship:
-    """
-    Descrição
-    ------
-    A classe encapsula a lógica de movimento dos navios.
 
-    Atributos
-    ---------
-    _position : pygame.Vector2
-        posição central do navio
-    _sprite : pygame.Surface
-        referência a usado para renderizar a imagem que representa o
-        navio
-    _speed : int
-        taxa de mudança de posição em pixel/s
-    _angle : float
-        ângulo da direção do navio com o eixo x
-    _angular_speed : float
-        taxa de mudança de ângulo em grau/s
+    SHIP_STATS = {
 
-    Propriedades
-    ------------
-    direction : pygame.Vector2
-        retorna a direção do movimento do navio
-    angle : float
-        retorna o valor de `_angle`
-    position : pygame.Vector2
-        retorna o valor de `_position`
-    speed : float
-        retorna o valor de `_speed`
-    angular_speed : float
-        retorna o valor de `_angular_speed`
+        2: {
+            "Turning Rate": 24,
+            "Top Speed": "%.2f" % 40,
+            "Speed Targets": [0, 30, 60, 90, 120],
+            "Acceleration": 1.5
+        }
+    }
 
-    Métodos
-    -------
-    move(dt):
-        muda a posição do navio
-    rotate(dt, reverse):
-        muda o ângulo do navio
-    """
+    def __init__(self, pos: (list, tuple, pg.Vector2), stage, gun_type, guns, sprite: pg.Surface, **kwargs):
 
-    def __init__(self, pos: (list, tuple, pg.Vector2), sprite: pg.Surface, **kwargs):
-        """
-        Descrição
-        ---------
-        Inicializa uma instância de Ship
-
-        Parâmetros
-        ----------
-        pos : list, tuple, pygame.Vector2
-            posição central do navio
-        speed : int, opcional
-            taxa de mudança de posição do navio em pixel/s
-            (default 250)
-        angle : float, opcional
-            ângulo do navio com o eixo x (default 0)
-        """
         self._position         = pg.Vector2(*pos)
         self._original_sprite  = sprite
-        self._speed            = kwargs.get('speed', 250)
         self._angle            = kwargs.get('angle', 0)
-        self._angular_speed    = kwargs.get('angular_speed', 50)
         self._render_sprite    = pg.transform.rotate(self._original_sprite, self._angle)
+
+
+        self.stage = stage
+        self.stats = Ship.SHIP_STATS[self.stage]
+
+        self._angular_speed = self.stats["Turning Rate"]
+
+        self.speed = 0
+        self.speed_target = 0
+        self.speed_target_list = self.stats["Speed Targets"]
+        self.acceleration = self.stats["Acceleration"]
+
+        self.guns = [Battery(self, i, gun_type) for i in range(guns)]
 
     @property
     def direction(self):
@@ -82,53 +50,49 @@ class Ship:
         return self._position
 
     @property
-    def speed(self):
-        return self._speed
-
-    @property
-    def angular_speed(self):
-        return self._angular_speed
-
-    @property
     def sprite(self):
         return self._render_sprite
 
     def move(self, dt: float):
-        """
-        Descrição
-        ------
-        Altera a posição do navio
-
-        Parâmetros
-        ----------
-        dt : float
-            variação de tempo
-
-        Retorno
-        -------
-        None
-        """
-        self._position.x += self.direction.x * self._speed * dt
-        self._position.y -= self.direction.y * self._speed * dt
+        self._position.x += self.direction.x * self.speed * dt
+        self._position.y -= self.direction.y * self.speed * dt
 
     def rotate(self, dt: float, reverse: bool = False):
-        """
-        Descrição
-        ---------
-        Altera o ângulo do navio com o eixo x
-
-        Parâmetros
-        ----------
-        angle : float
-            variação do ângulo do navio
-
-        Retorno
-        -------
-        None
-        """
         mod = -1 if reverse else 1
         self._angle += self._angular_speed * mod * dt
         self._angle %= 360
 
-        self._render_sprite = pg.transform.rotate(self._original_sprite, self._angle)
+    def change_speed(self):
+        if self.speed < self.speed_target_list[self.speed_target]:
+            self.speed = min(self.speed_target_list[self.speed_target], self.speed + self.acceleration)
+        elif self.speed > self.speed_target_list[self.speed_target]:
+            self.speed = max(self.speed_target_list[self.speed_target], self.speed - self.acceleration)
 
+    def increase_speed(self):
+        if self.speed_target < 4:
+            self.speed_target += 1
+
+    def decrease_speed(self):
+        if self.speed_target > 0:
+            self.speed_target -= 1
+
+    def update(self, dt, event):
+        if event.type == pg.KEYDOWN:
+
+            if event.key == pg.K_w:
+                self.increase_speed()
+            elif event.key == pg.K_s:
+                self.decrease_speed()
+
+            elif event.key == pg.K_a:
+                self.rotate(dt, True)
+            elif event.key == pg.K_d:
+                self.rotate(dt)
+
+        self.change_speed()
+        self.move(dt)
+
+        self._render_sprite = pg.transform.rotate(self._original_sprite, self._angle)
+        target = pg.mouse.get_pos()
+        for gun in self.guns:
+            self._render_sprite = gun.ready_aim(self._render_sprite, target, dt)
