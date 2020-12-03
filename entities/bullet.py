502bdf2611490937cfc5abb4141a6c24f2964691
Lucca Limongi <laldf@cin.ufpe.br>
2020-12-03 13:58:22 -0300

    Armas consertadas

--- entities/bullet.py
+++ entities/bullet.py
@@ -13,16 +13,17 @@ class Bullet(object):
     def __init__(self, position, angle, caliber):
         self.x, self.y = position
         self.angle = angle
-
+        print(self.angle)
         self.lifetime, self.speed = Bullet.BULLET_DATA[caliber]
 
-        self.x_vel = round(math.cos(self.angle) * self.speed, 2)
-        self.y_vel = round(math.sin(self.angle) * self.speed, 2)
+        self.x_vel =  math.cos(math.radians(self.angle)) * self.speed
+        self.y_vel = -math.sin(math.radians(self.angle)) * self.speed
+
+        print(self.x_vel, self.y_vel)
 
     def travel(self, dt):
-        self.x = self.x + self.x_vel * dt
-        self.y = self.y + self.y_vel * dt
-
+        self.x += self.x_vel * dt
+        self.y += self.y_vel * dt
         self.lifetime -= dt
 
         if self.lifetime > 0:
