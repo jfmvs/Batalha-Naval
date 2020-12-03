6d1595e37bab824c3e135ffffa491a363b704377
artn-dev <azemarteixeira@gmail.com>
2020-12-03 20:48:10 -0300

    removi zoom

--- entities/camera.py
+++ entities/camera.py
@@ -3,9 +3,8 @@ from .navios import Ship
 
 
 class Camera:
-    def __init__(self, pos: (list, tuple), size, zoom=1.0, speed=150):
+    def __init__(self, pos: (list, tuple), size, speed=150):
         self._position = pg.Vector2(pos)
-        self._zoom     = zoom
         self._speed    = speed
         self.width, self.height = size
 
@@ -17,25 +16,6 @@ class Camera:
     def position(self, value):
         self._position.x = value[0]
         self._position.y = value[1]
-
-    @property
-    def zoom(self):
-        return self._zoom
-
-    @zoom.setter
-    def zoom(self, value: float):
-        if value > Camera._ZOOM_MAX:
-            self._zoom = Camera._ZOOM_MAX
-        elif value < Camera._ZOOM_MIN:
-            self._zoom = Camera._ZOOM_MIN
-        else:
-            self._zoom = value
-
-    def zoom_in(self):
-        self.zoom += 0.01
-
-    def zoom_out(self):
-        self.zoom -= 0.01
 
     def center(self, target: Ship):
         self._position = target.position
