b5d9346953617e69b2fa931a955785fc1a11b301
artn-dev <azemarteixeira@gmail.com>
2020-12-02 23:44:26 -0300

    corrigi produtos

--- core/bullet.py
+++ core/bullet.py
@@ -21,25 +21,12 @@ class Bullet(object):
         self.y_vel = round(math.sin(self.angle) * self.speed, 2)
 
     def travel(self, dt):
-        self.x = self.x + self.x_vel / dt
-        self.y = self.y + self.y_vel / dt
+        self.x = self.x + self.x_vel * dt
+        self.y = self.y + self.y_vel * dt
 
-        self.lifetime -= 1 / dt
+        self.lifetime -= dt
 
         if self.lifetime > 0:
             return True
         else:
             return False
-
-
-    for event in pygame.event.get():
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if pygame.mouse.get_pressed()[0]:
-                mouse_x, mouse_y = pygame.mouse.get_pos()
-                bullets.append(bullet(x, y, mouse_x, mouse_y))
-
-    for bullet_ in bullets:
-        bullet_.draw(win, fps)
-        if bullet_.lifetime <= 0:
-            bullets.pop(bullets.index(bullet_))
