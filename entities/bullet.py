7f390b70310e7518609f2852fcc4cf60f4a9a1a4
Lucca Limongi <laldf@cin.ufpe.br>
2020-12-03 13:59:23 -0300

    Removendo prints desnecessários

--- entities/bullet.py
+++ entities/bullet.py
@@ -13,13 +13,10 @@ class Bullet(object):
     def __init__(self, position, angle, caliber):
         self.x, self.y = position
         self.angle = angle
-        print(self.angle)
         self.lifetime, self.speed = Bullet.BULLET_DATA[caliber]
 
         self.x_vel =  math.cos(math.radians(self.angle)) * self.speed
         self.y_vel = -math.sin(math.radians(self.angle)) * self.speed
-
-        print(self.x_vel, self.y_vel)
 
     def travel(self, dt):
         self.x += self.x_vel * dt
